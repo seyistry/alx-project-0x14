05e93d2f10253833f4ed2c7ece591219ca208aaf
Oluwaseyi <seyistry@gmail.com>
2025-08-10 16:28:56 +0100

    update

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-	<div>Layout</div>
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
   )
 }
 
-export default Layout
+export default Layout;
